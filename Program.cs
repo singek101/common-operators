1b3a84a490a8367f7692f91e7607fae1cc1beed5
singek101 <michaelmusediku@gmail.com>
2021-04-20 10:17:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace common_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Arthmentic operators + - / % *
+            //terary opreators 
+            int x = 12;
+            int y = 11;
+            int  result = x < y ? x + y : x++;
+            Console.WriteLine(result);
+        }
+    }
+}
